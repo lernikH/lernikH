d23d80e58dcd70614cddaef0cc78762f76340443
lernikH <lernikh10@gmail.com>
2024-03-13 21:20:56 +0400

    store_server

---
+++ products/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import fields, serializers
+
+from products.models import Basket, Product, ProductCategory
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field='name', queryset=ProductCategory.objects.all())
+
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'description', 'price', 'quantity', 'image', 'category')
+
+
+def get_total_sum(obj):
+    return Basket.objects.filter(user_id=obj.user.id).total_sum()
+
+
+def get_total_quantity(obj):
+    return Basket.objects.filter(user_id=obj.user.id).total_quantity()
+
+
+class BasketSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+    sum = fields.FloatField(required=False)
+    total_sum = fields.SerializerMethodField()
+    total_quantity = fields.SerializerMethodField()
+
+    class Meta:
+        model = Basket
+        fields = ('id', 'product', 'quantity', 'sum', 'total_sum', 'total_quantity', 'created_timestamp')
+        read_only_fields = ('created_timestamp',)
+
---
+++ users/views.py
@@ -0,0 +1,75 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, HttpResponseRedirect
+from users.models import User
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from django.contrib import auth, messages
+from django.urls import reverse
+from products.models import Basket
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Շնորհավորում բարեհաջող գրանցման համար')
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.users, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    baskets = Basket.objects.filter(user=request.user)
+    total_sum = sum(basket.sum() for basket in baskets)
+    total_quantity = sum(basket.quantity for basket in baskets)
+    context = {'title': 'Store-Профиль',
+               'form': form,
+               'baskets': Basket.objects.all(),
+               'total_sum': total_sum,
+               'total_quantity': total_quantity,
+
+               }
+    return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+@login_required
+def basket_add(request, product_id):
+    Basket.create_or_update(product_id, request.user)
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+@login_required
+def basket_remove(request, basket_id):
+    basket = Basket.objects.get(id=basket_id)
+    basket.delete()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
